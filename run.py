c9d058a0125f7673f889433fe1eab7470cadde78
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-01-27 20:05:56 +0000

    clean up

--- run.py
+++ run.py
@@ -103,7 +103,6 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
     update_surplus_worksheet(new_surplus_data)
 
 
